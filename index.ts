08bcc83136e909a50bb5924f698237983fe5b977
tehreembano <164682815+tehreembano@users.noreply.github.com>
2024-04-12 03:34:11 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter Second number", type: "number", name: "SecondNumber" },
+  {
+    message: "Sclect one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Substraction", "Multiplication", "Division"],
+  },
+]);
+console.log(answer);
+//conditional statement
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.SecondNumber);
+} else if (answer.operator === "Substraction") {
+  console.log(answer.firstNumber - answer.SecondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.SecondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.SecondNumber);
+} else {
+  console.log("Please select valid operator");
+}
